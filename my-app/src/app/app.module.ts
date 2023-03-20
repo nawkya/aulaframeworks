b1b7097bf3853e634dcea5d9a276cfbf86d2eab3
ana <ana.camargo5@estudante.ifms.edu.br>
2023-03-20 16:46:44 -0400

    dificilbotao

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+export { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
@@ -15,4 +15,18 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+
+export class AppComponent{
+  nome = 'beto';
+  count = 10;
+
+
+
+ menos(){
+  this.count --;
+}
+
+ mais(){
+  this.count ++;
+}
+}
