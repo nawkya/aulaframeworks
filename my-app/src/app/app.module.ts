858db8e25a45473bb3d4e8191cd487263fd984c7
vxizza <izadoraisidoro145@gmail.com>
2023-04-06 14:46:46 -0400

    login completo

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroComponent } from './components/hero/hero.component';
 import { LoginComponent } from './components/login/login.component';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -15,7 +16,8 @@ import { LoginComponent } from './components/login/login.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- my-app/src/app/components/login/login.component.ts
+++ my-app/src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TitleStrategy } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,6 +7,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-  isError= false;
+  isError = false;
+  isSucess = false;
+
+  user = ""
+  pwd = ""
+
+
+
+
+  login() {
+
+    console.log("User = " + this.user + " Password = " + this.pwd)
+    if (
+      this.user == "ana.com" && this.pwd == "naju"
+    ) {
+      console.log("login aprovado!")
+      this.isError = false;
+      this.isSucess = true;
+    }
+
+    else {
+      this.isError = true;
+      this.isSucess = false;
+    }
+  }
 
 }
