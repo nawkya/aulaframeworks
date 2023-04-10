2cf45f34478d72b1ea041917eb76d6e33d0aa265
ana <ana.camargo5@estudante.ifms.edu.br>
2023-04-10 16:46:50 -0400

    rotas

--- my-app/src/app/app-routing.module.ts
+++ my-app/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateUserComponent } from './components/create-user/create-user.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { LoginComponent } from './components/login/login.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+ {path: 'heroes', component: HeroComponent},
+ {path:'login', component: LoginComponent},
+ {path: 'create', component: CreateUserComponent},
+ {path:'', redirectTo:'/login', pathMatch:'full'},
+ {path:'**', component: PageNotFoundComponent},
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
